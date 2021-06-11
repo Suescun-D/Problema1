c98f6bfa87f121c96a0172485558d1d8db36824b
Suescun-D <76526997+Suescun-D@users.noreply.github.com>
2021-06-10 19:11:31 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+function csvtoarray($archivo,$delimitador = ","){
+
+	if(!empty($archivo) && !empty($delimitador) && is_file($archivo)):
+
+		$array_total = array();
+
+		$fp = fopen($archivo,"r");
+
+		while ($data = fgetcsv($fp, 10000, $delimitador)){
+
+			$num = count($data);
+
+			$array_total[] = array_map("utf8_encode",$data);
+
+		}
+
+		fclose($fp);
+
+		return $array_total;
+
+	else:
+
+		return false;
+
+	endif;
+
+}
+
+$arraycsv = csvtoarray("Problema1.csv");
+
+print_r($arraycsv);
+
+// foreach ($arraycsv as $key => $value) {
+//    print $key;
+// }
+
+?> 
